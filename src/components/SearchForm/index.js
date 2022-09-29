4fcdfed0dea2a50c744b0cd4266cb9dee3d57cad
Rami Elsayed <ramielsayed123@outlook.com>
2022-09-29 19:11:54 +0100

    intial search func useEffect and state

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,15 +1,38 @@
+import { useEffect, useState } from "react";
+import classNames from "classnames";
 import "../../styles.css";
 export const SearchForm = () => {
+  const [isLoading, setisLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    if (searchTerm) {
+      console.log("Make Api request for", searchTerm);
+    }
+  }, [searchTerm]);
   return (
     <form
       className="aside-item"
       onSubmit={(event) => {
         event.preventDefault();
-        console.log("make API request");
+        setSearchTerm(inputValue);
       }}
     >
-      <div className="ui icon input">
-        <input type="text" placeholder="Enter city name" />
+      <div
+        className={classNames("ui icon input", {
+          disabled: isLoading,
+          loading: isLoading,
+        })}
+      >
+        <input
+          value={inputValue}
+          onChange={(event) => {
+            setInputValue(event.currentTarget.value);
+          }}
+          type="text"
+          placeholder="Enter city name"
+        />
         <i className="search icon"></i>
       </div>
     </form>
